1b981ab6312dcecbee30cf307f84440378d95b74
Hunain <hunain.chhipa23@gmail.com>
2024-11-05 19:29:27 +0530

    added security middlewares

--- 04-jobs-app/server/middleware/error-handler.js
+++ 04-jobs-app/server/middleware/error-handler.js
@@ -1,11 +1,41 @@
-const { CustomAPIError } = require("../errors");
+// const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const erroHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  let customError = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong try again later",
+  };
+
+  // if (err instanceof CustomAPIError) {
+  // return res.status(err.statusCode).json({ msg: err.message });
+  // }
+
+  // validation errors message for every value
+  if (err.name === "ValidationError") {
+    console.log(Object.values(err.errors));
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+    customError.statusCode = 400;
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = 400;
+  }
+
+  // cast error message
+  if (err.name === "CastError") {
+    customError.msg = `No item found with id: ${err.value}`;
+    customError.statusCode = 404;
+  }
+
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err }); // return internal server error if there is any type of error
+  return res.status(customError.statusCode).json({ msg: customError.msg }); // return custom error messages
 };
 
 module.exports = erroHandlerMiddleware;
